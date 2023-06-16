26d5ed5395a79fa5dfc9520b997770484e518211
4lg4 <akgleal@gmail.com>
2023-06-17 07:55:27 +0930

    stich raw 360 and add missing meta data

--- src/gen.sh
+++ src/gen.sh
@@ -1,32 +1,63 @@
 #!/bin/bash
 
+FOLDER_INPUT="/Users/paulaceron/ALGA/dev/360-video-builder/files/input"
+FOLDER_OUTPUT="/Users/paulaceron/ALGA/dev/360-video-builder/files/input"
+COUNT=$(ls $FOLDER_OUTPUT/final*.mp4 | wc -l | sed 's/ //g')
 LENS_ANGLE=188
 VIDEO_WIDTH=3840
 VIDEO_HEIGHT="1920"
-VIDEO="./2.insv"
-COUNT=$(ls final* | wc -l | sed 's/ //g')
+VIDEO_METATAGS="$FOLDER_INPUT/data_tags.mp4"
+
+# VIDEO_INPUT="\"concat:$FOLDER_INPUT/1.insv|$FOLDER_INPUT/2.insv\""
+VIDEO_FILES="$FOLDER_INPUT/files.txt"
+echo "" > $VIDEO_FILES
+# for f in $FOLDER_INPUT/*.insv; do echo "file '$FOLDER_INPUT/$f' >> $VIDEO_FILES"; done
+echo "file $FOLDER_INPUT/1.insv" >> $VIDEO_FILES
+echo "file $FOLDER_INPUT/2.insv" >> $VIDEO_FILES
+VIDEO_INPUT="$VIDEO_FILES"
+
+VIDEO_OUTPUT="$FOLDER_INPUT/final_$COUNT.mp4"
+MERGE_MAP="$FOLDER_INPUT/mergemap.png"
+FFMPEG="/Users/paulaceron/ALGA/dev/360-video-builder/node_modules/@ffmpeg-installer/darwin-arm64/ffmpeg"
 YAW=180
 
 createMergeMap () {
   V360="v360=input=fisheye:output=e:ih_fov=$LENS_ANGLE:iv_fov=$LENS_ANGLE"
 
-  /Users/paulaceron/ALGA/dev/360-video-builder/node_modules/@ffmpeg-installer/darwin-arm64/ffmpeg -f lavfi \
+  $FFMPEG -f lavfi \
       -i nullsrc=size="${VIDEO_HEIGHT}x$VIDEO_HEIGHT" \
       -vf "format=gray8,geq='clip(128-128/(6)*($YAW-$LENS_ANGLE/($VIDEO_HEIGHT/2)*hypot(X-$VIDEO_HEIGHT/2,Y-$VIDEO_HEIGHT/2)),0,255)',$V360,format=rgb24" \
-      -frames 1 -y mergemap.png
+      -frames 1 -y $MERGE_MAP
 }
 
+# cd $FOLDER_INPUT && \
+# pwd && FUCK && \
 # createMergeMap && FUCK && \
+# createMergeMap && \
 
-createMergeMap && \
+COMMAND="$FFMPEG \
+-i $VIDEO_INPUT -i $MERGE_MAP -profile:v High -pix_fmt yuv420p \
+-lavfi \"[0]format=rgb24,split[a][b];[a]crop=ih:iw/2:0:0, v360=input=fisheye:output=e:ih_fov=$LENS_ANGLE:iv_fov=$LENS_ANGLE:roll=90[c]; [b]crop=ih:iw/2:iw/2:0,v360=input=fisheye:output=e:yaw=$YAW:ih_fov=$LENS_ANGLE:iv_fov=$LENS_ANGLE:roll=90[d]; [c][d][1]maskedmerge,format=rgb24\" \
+-y $VIDEO_OUTPUT && \ "
 
-/Users/paulaceron/ALGA/dev/360-video-builder/node_modules/@ffmpeg-installer/darwin-arm64/ffmpeg \
--i $VIDEO -i mergemap.png -profile:v High -pix_fmt yuv420p \
--lavfi "[0]format=rgb24,split[a][b];[a]crop=ih:iw/2:0:0, v360=input=fisheye:output=e:ih_fov=$LENS_ANGLE:iv_fov=$LENS_ANGLE:roll=90[c]; [b]crop=ih:iw/2:iw/2:0,v360=input=fisheye:output=e:yaw=$YAW:ih_fov=$LENS_ANGLE:iv_fov=$LENS_ANGLE:roll=90[d]; [c][d][1]maskedmerge,format=rgb24" \
--y ./final_$COUNT.mp4 && \
+echo $COMMAND
+# $($COMMAND)
 
-# /Users/paulaceron/ALGA/dev/360-video-builder/node_modules/@ffmpeg-installer/darwin-arm64/ffmpeg -i $VIDEO \
-# -vf v360=dfisheye:e:ih_fov=184:iv_fov=184:roll=-180 \
-# final_$COUNT.mp4 && \
-exiftool -tagsFromFile data_tags.mp4 -all:all final_$COUNT.mp4 && \
-open -a "VLC" ./final_$COUNT.mp4
+COMMAND_SINGLE="$FFMPEG -f concat -safe 0 -i $VIDEO_INPUT \
+ -vf v360=dfisheye:e:ih_fov=$LENS_ANGLE:iv_fov=$LENS_ANGLE:roll=-180 \
+ $VIDEO_OUTPUT"
+
+echo  "------"
+echo $COMMAND_SINGLE
+echo  "------"
+# cd $FOLDER_INPUT
+# echo  "------"
+# pwd
+# echo  "------"
+
+$($COMMAND_SINGLE)
+
+function injectMetadata() {
+# exiftool -tagsFromFile $VIDEO_METATAGS -all:all $VIDEO_OUTPUT && \
+# open -a "VLC" $VIDEO_OUTPUT
+}
